235b8ebf7ca33b550253b21046b2dd390c7bdc72
Hashmat Abbas <hashmat@graffitecs.com>
2019-08-20 14:22:13 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Switch from '@material-ui/core/Switch';
 
 function App() {
+  const [switchState, setSwitchState] = useState([]);
+  function handleChange(id, e) {
+    let current = [...switchState];
+    if (e.target.checked) {
+      current.push(id);
+    } else {
+      debugger;
+      var index = current.indexOf(id);
+      if (index > -1) {
+        current.splice(index, 1);
+      }
+    }
+    setSwitchState(current);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Switches example</p>
+        <Switch
+          checked={switchState.includes('A')}
+          onChange={e => handleChange('A', e)}
+          value="A"
+        />
+        <Switch
+          checked={switchState.includes('B')}
+          onChange={e => handleChange('B', e)}
+          value="B"
+        />
+        <Switch
+          checked={switchState.includes('C')}
+          onChange={e => handleChange('C', e)}
+          value="C"
+        />
+        <Switch
+          checked={switchState.includes('D')}
+          onChange={e => handleChange('D', e)}
+          value="D"
+        />
       </header>
     </div>
   );
